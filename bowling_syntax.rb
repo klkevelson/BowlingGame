ae4947b90feff30842f8c02ecd22a415d511cdae
klkevelson <klkevelson@gmail.com>
2016-05-18 12:14:47 -0400

    bowling_syntax.rb
    An updated version of the syntax using a simple while loop.

---
+++ bowling_syntax.rb
@@ -0,0 +1,28 @@
+puts "Welcome to bowling. Would you like to bowl ten frames? Type Y for yes and N for no."
+i = 0
+j = 0
+c = gets
+
+if c.strip == "Y"
+  frames = Array.new(10) { Array.new(2) }
+  
+  while (i<10) do
+    puts "Are you ready to bowl frame number #{(i+1)}? "
+    choice = gets
+    choice.strip!
+    
+    if((choice == "Y")&&(j<2))
+      frames[i,j] = rand(10) 
+      j = j+1
+      puts "The roll was ball number #{(j+1)} of frame number #{(i+1)} is #{frames[i,j]} \n" 
+    elsif ((choice == "Y")&&(j==2))
+      j = 0
+      i = i + 1
+      frames[i,j] = rand(10)
+      puts "The roll was ball number #{(j+1)} of frame number #{(i+1)} is #{frames[i,j]} \n" 
+    end
+  end
+else
+ puts "Then have a nice day!"
+ 
+end
